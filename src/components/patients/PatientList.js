790dd82d040be21c8dd090697be0c1f2cf50c392
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-05-24 18:11:43 -0400

    only allow orgs to see verified users. allow social workers to see all patients. do not allow patients to see other patients

--- src/components/patients/PatientList.js
+++ src/components/patients/PatientList.js
@@ -9,17 +9,31 @@ class PatientList extends Component {
 		this.props.fetchPatients();
 	}
 
+	filteredPatients() {
+		if (this.props.currentUser.role === 'organization') {
+			return this.props.patients.filter(patient => {
+				return patient.verified && patient.verified === true
+			});
+		} else if (this.props.currentUser.role === 'social_worker') {
+			return this.props.patients;
+		} else {
+			return [];
+		}
+	}
+
 	renderPatients() {
-		if (this.props.patients) {
+		if (this.props.currentUser && this.props.patients) {
 			return (
 				<ul>
 				{
-					this.props.patients.map((patient, index) => {
+					this.filteredPatients().map((patient, index) => {
 						return (<li><Link to={`/patients/${patient._id}`} className="well" key={index}>{patient.email}</Link></li>)
 					})
 				}
 				</ul>
 			);
+		} else {
+			return <h1>You are not authorized to see this page</h1>
 		}
 	}
 
@@ -36,7 +50,8 @@ class PatientList extends Component {
 
 function mapStateToProps(state) {
 	return {
-		patients: state.patients.patients
+		patients: state.patients.patients,
+		currentUser: state.auth.user
 	}
 }
 
