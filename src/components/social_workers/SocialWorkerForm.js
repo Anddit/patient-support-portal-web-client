e28a529bc1c9ec16f8d527b94056ce5d92f6b9e7
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-05-24 19:22:58 -0400

    add the social worker form

---
+++ src/components/social_workers/SocialWorkerForm.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+import * as actions from '../../actions/index.js';
+
+class SocialWorkerForm extends Component {
+
+	handleFormSubmit({email, name, indication}) {
+		// Need to do something to log user in
+		this.props.updateUser(
+			this.props._id, 
+			{ email, name, contact, website, description, indications_served, services },
+			'social_worker'
+		);
+	}
+
+
+	renderAlert() {
+		if (this.props.errorMessage) {
+			return (
+				<div className="alert alert-danger">
+					<strong>Oops!</strong> {this.props.errorMessage}
+				</div>
+			);
+		}
+	}
+
+
+	render() {
+		const { handleSubmit, fields: { email, name, contact, website, description, indications_served, services }} = this.props;
+
+		return (
+			<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+				<fieldset className="form-group">
+					<label>Email:</label>
+					<input {...email} className="form-control" />
+				</fieldset>
+
+				<fieldset className="form-group">
+					<label>Name:</label>
+					<input {...name} className="form-control" />
+				</fieldset>
+
+				<fieldset className="form-group">
+					<label>Contact Info:</label>
+					<input {...contact} className="form-control" />
+				</fieldset>
+
+				<fieldset className="form-group">
+					<label>Website URL:</label>
+					<input {...website} className="form-control" />
+				</fieldset>							
+
+				<fieldset className="form-group">
+					<label>Description:</label>
+					<textarea {...description} className="form-control"></textarea>
+				</fieldset>
+
+				<fieldset className="form-group">
+					<label>Indications Served:</label>
+					<textarea {...indications_served} className="form-control"></textarea>
+				</fieldset>
+
+				<fieldset className="form-group">
+					<label>Services Offered:</label>
+					<textarea {...services} className="form-control"></textarea>
+				</fieldset>				
+												
+				{this.renderAlert()}
+				<button action="submit" className="btn btn-primary">Save</button>
+			</form>
+		);
+	}
+
+}
+
+function mapStateToProps(state) {
+	return { 
+		errorMessage: state.patients.error,
+		currentUser: state.auth.user
+	 };
+}
+
+export default reduxForm({
+	form: 'organization',
+	fields: ['email', 'name', 'contact', 'website', 'description', 'indications_served', 'services']
+}, mapStateToProps, actions)(SocialWorkerForm);
